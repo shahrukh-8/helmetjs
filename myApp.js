54ab53fb569b680f04bcc1906cc7ba9e32fbde52
Shahrukh <shahrukh8@live.com.sg>
2024-10-29 16:39:09 +0800

    add frameguard

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy())
-
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard(
+  { action: 'deny' }
+));
 
 
 
