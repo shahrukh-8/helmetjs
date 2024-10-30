75bba59999171c26fbc86cc54a1b5bc16fdd94c4
Shahrukh <shahrukh8@live.com.sg>
2024-10-30 09:53:44 +0800

    add hsts

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
-ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+timeInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
 
 
